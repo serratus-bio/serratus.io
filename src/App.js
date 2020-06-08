9fe5a25456b74292c2e49d41325786a83851f8f5
lohrd <57124913+lohrd@users.noreply.github.com>
2020-06-08 14:14:51 -0400

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
 import Navbar from './components/Navbar'
 import './styles/main.css';
 
 const App = () => {
   return (
-    <Navbar>
-
-    </Navbar>
+    <>
+    <Navbar></Navbar>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Projects from '../pages/Projects'
 
 const Navbar = () => {
     return (
-        <div>
-            <p>Navbar</p>
-            <NavLink
-            exact
-            to="/"
-            className="nav-item nav-link"
-            activeClassName="nav-item nav-link active">Home</NavLink>
-          <NavLink
-            exact
-            to="/"
-            className="nav-item nav-link"
-            activeClassName="nav-item nav-link active">About</NavLink>
-          <NavLink
-            exact
-            to="/"
-            className="nav-item nav-link"
-            activeClassName="nav-item nav-link active">Projects</NavLink>
-        </div>
+        <Router>
+            <div className="flex flex-row bg-teal-500 p-4 justify-between">
+                <p className="ml-10">Navbar</p>
+                <div className="justify-flex-end mr-10">
+                    <NavLink exact to="/" className="ml-10 hover:text-blue-800" activeClassName="text-blue-600">Home</NavLink>
+                    <NavLink exact to="/About" className="ml-10 hover:text-blue-800" activeClassName="text-blue-600">About</NavLink>
+                    <NavLink exact to="/Projects" className="ml-10 hover:text-blue-800" activeClassName="text-blue-600">Projects</NavLink>
+                </div>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/About">
+                    <About/>
+                </Route>
+                <Route exact path="/Projects">
+                    <Projects/>
+                </Route>
+            </Switch>
+    </Router>
     )
 }
 
