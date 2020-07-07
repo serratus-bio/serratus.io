f92ddda1a7cfd7d33ebb67d153044059ffc02c8e
lohrd <57124913+lohrd@users.noreply.github.com>
2020-07-07 18:40:51 -0400

    Update Data.js

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Axios from 'axios';
 import DataSdk from '../SDK/DataSdk';
+import { get } from 'react-scroll/modules/mixins/scroller';
 
 const  Data = (props) => {
     const [searchString, setSearchString] = React.useState("")
     const [data, setData] = React.useState({});
     const [heatMap, setHeatMap] = React.useState();
+    const [imageError, setImageError] = React.useState("");
+
+    const genSra = () => {
+        getData(searchString)
+        getImage(searchString)
+    }
 
     const getData = async (searchString) => {
         const dataSdk = new DataSdk();
@@ -16,16 +23,16 @@ const  Data = (props) => {
 
     const getImage = async (searchString) => {
         const dataSdk = new DataSdk();
-        const img = await dataSdk.getSraHeatMapByName(searchString);
-        // .then(response => {
-        //     let blob = new Blob(
-        //       [response.data], 
-        //       { type: response.headers['content-type'] }
-        //     )
-        //     let image = URL.createObjectURL(blob);
-        let sraHeatMap = URL.createObjectURL(img)
-        setHeatMap(sraHeatMap)
-        console.log(sraHeatMap);
+        try {
+            const img = await dataSdk.getSraHeatMapByName(searchString);
+            let sraHeatMap = URL.createObjectURL(img)
+            setHeatMap(sraHeatMap)
+            console.log(sraHeatMap);
+        }
+        catch {
+            setImageError("No heat map for this SRA")
+        }
+
     }
     
     return (
@@ -35,11 +42,12 @@ const  Data = (props) => {
         //     </div>
         //     :
         <div className="h-screen flex flex-col items-center bg-blue-500 justify-center">
-        {heatMap ? <img src={heatMap} className="h-auto w-auto"></img> : <div>no image</div> }
+        {data ? <div>data</div> : <div>no data</div>}
+        {heatMap ? <img src={heatMap} className="h-auto w-auto"></img> : <div>{imageError}</div> }
             
             <input onChange={e => setSearchString(e.target.value)}></input>
 
-            <button onClick={() => getImage(searchString)} className="h-10 w-24 bg-gray-500" title="get heat">get heat map</button>
+            <button onClick={() => genSra()} className="h-10 w-24 bg-gray-500" title="get heat">get heat map</button>
             
        
         </div>
