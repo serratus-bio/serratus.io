953ab38eba1dfb42584d37f666b57ff1d63758a2
Victor Lin <victor@victorl.in>
2020-07-09 16:28:12 -0400

    Update report page (#10)
    - parameterize accession in URL
    - links to SRA, Trace, BAM
    - update search field
    - preliminary parsing of summary file info

--- src/SDK/DataSdk.js
+++ src/SDK/DataSdk.js
@@ -14,6 +14,11 @@ export default class TrackSdk {
         return response.data
     }
 
+    async fetchAccessionJSON(accession) {
+        const response = await axios.get(`https://api.serratus.io/api/summary/${accession}`);
+        return response.data;
+    }
+
     async getSraHeatMapByName(sraName) {
         const response = await axios.get(`https://api.serratus.io/api/summary/${sraName}/coverage_heatmap.png`, { responseType: 'blob' });
             return response.data;
@@ -27,4 +32,42 @@ export default class TrackSdk {
         const response = await axios.delete(`http://localhost:3000/tracks/${track.trackId}`);
         return response.data
     }
+
+    async getEsearch(accession) {
+        const response = await axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=${accession}&retmax=1&usehistory=y`, {responseType: 'text'});
+        return response.data;
+    }
+
+    async getEsummary(entrezId) {
+        const response = await axios.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=sra&id=${entrezId}`, {responseType: 'text'});
+        return response.data;
+    }
+
+    async getEntrezData(sra_accession) {
+        // eSearch
+        let response = await this.getEsearch(sra_accession);
+        let parser = new DOMParser();
+        let esearchResults = parser.parseFromString(response, 'text/xml');
+        let entrezId = esearchResults
+            .querySelector("eSearchResult")
+            .querySelector("IdList")
+            .querySelector("Id")
+            .textContent;
+        // eSummary
+        response = await this.getEsummary(entrezId);
+        let esummaryResults = parser.parseFromString(response, 'text/xml');
+        let expXmlText = esummaryResults
+            .querySelector("eSummaryResult")
+            .querySelector("DocSum")
+            .querySelector("Item") // first Item
+            .textContent;
+        // eSummary expXml
+        expXmlText = '<tag>' + expXmlText + '</tag>';
+        let expXml = parser.parseFromString(expXmlText, 'text/xml');
+        let entrezStudyName = expXml
+            .getRootNode()
+            .querySelector('Study')
+            .getAttribute('name')
+        return entrezStudyName;
+      }
 }
--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,62 +1,98 @@
-import React from 'react';
-import Axios from 'axios';
+import React from "react";
 import DataSdk from '../SDK/DataSdk';
-import { get } from 'react-scroll/modules/mixins/scroller';
 
-const  Data = (props) => {
-    const [searchString, setSearchString] = React.useState("")
-    const [data, setData] = React.useState({});
+const Data = (props) => {
+    var accessionFromParam = new URLSearchParams(props.location.search).get("accession");
+    const [sraAccession, setAccession] = React.useState(accessionFromParam);
+    const [inputAccession, setInputAccession] = React.useState("");
     const [heatMap, setHeatMap] = React.useState();
-    const [imageError, setImageError] = React.useState("");
+    const [summaryJson, setSummaryJson] = React.useState({});
+    const [entrezStudyName, setEntrezStudyName] = React.useState("Loading...");
+    const dataSdk = new DataSdk();
 
-    const genSra = () => {
-        getData(searchString)
-        getImage(searchString)
-    }
-
-    const getData = async (searchString) => {
-        const dataSdk = new DataSdk();
-        const sraData = await dataSdk.getSraByName(searchString); 
-        setData(sraData);  
-        console.log(sraData); 
-    }
-
-    const getImage = async (searchString) => {
-        const dataSdk = new DataSdk();
-        try {
-            const img = await dataSdk.getSraHeatMapByName(searchString);
-            let sraHeatMap = URL.createObjectURL(img)
-            setHeatMap(sraHeatMap)
-            console.log(sraHeatMap);
-        }
-        catch {
-            setImageError("No heat map for this SRA")
+    async function fetchSerratusApiData(ignore) {
+        console.log("Fetching Serratus API data...");
+        let summaryJson = await dataSdk.fetchAccessionJSON(sraAccession);
+        let heatMapData = await dataSdk.getSraHeatMapByName(sraAccession);
+        let sraHeatMap = URL.createObjectURL(heatMapData)
+        if (!ignore) {
+          setSummaryJson(summaryJson);
+          setHeatMap(sraHeatMap);
+          console.log("Done fetching Serratus API data.");
         }
     }
-    
-    return (
-        <div className="h-screen w-screen flex flex-col items-center bg-blue-500 justify-center p-6">
-            <div class="w-1/2 h-64 flex flex-col justify-center">
 
-           
-            <div class="flex flex-col justify-center items-center">
-                {data.sra 
-                ? 
-                <div>SRA: {data.sra}</div> 
-              
-                : 
-                <div>SRA: </div>}
-                {heatMap ? 
-                <img src={heatMap} className="h-full w-full p-6 m-6"></img>
-                : 
-                <div>{imageError}</div> }
-                <div class="flex flex-col justify-center items-center">
-                    <input class="p-2 border-black border-2 rounded-lg" onChange={e => setSearchString(e.target.value)}></input>
-                    <button class="" onClick={() => genSra()} className="h-10 w-32 border-black border-2 rounded-lg" title="get heat">Search</button>
+    async function fetchEntrezData(ignore) {
+        console.log("Fetching Entrez data...");
+        let entrezStudyName = await dataSdk.getEntrezData(sraAccession);
+        if (!ignore) {
+            setEntrezStudyName(entrezStudyName);
+            console.log("Done fetching Entrez data.");
+        }
+    }
+
+    React.useEffect(() => {
+        if (!sraAccession) {
+            return;
+        }
+        console.log(`Loading report page for ${sraAccession}.`);
+        let ignore = false;
+        fetchSerratusApiData(ignore);
+        fetchEntrezData(ignore);
+        return () => { ignore = true; }
+    }, []);
+
+    const redirect = accession => e => {
+        console.log(`/Data?accession=${accession}`);
+        window.location.href = `/Data?accession=${accession}`
+    }
+
+    let searchBox = (
+        <div class="flex flex-col items-center">
+            <div class="flex-row">
+                <input class="rounded border-2 border-gray-300 px-2 m-1" type="text" placeholder="Enter SRA Accession ID" onChange={e => setInputAccession(e.target.value)} />
+                <button onClick={redirect(inputAccession)} class="rounded bg-blue-500 text-white font-bold py-1 px-4" type="submit">Go</button>
+            </div>
+        </div>
+    )
+
+    let reportContent = (
+        <div class="flex flex-wrap">
+            <div class="w-full text-center mb-8">
+                <div class="text-2xl">{sraAccession} Report Page</div>
+            </div>
+            <div id="external-links" class="flex w-full mb-3">
+                <div class="w-1/4"></div>
+                <div class="w-1/6">
+                    <div><a class="text-blue-500" target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/sra/?term=${sraAccession}`}>SRA Link</a></div>
                 </div>
-                <a src={`https://www.ncbi.nlm.nih.gov/sra/?term=${data.sra}`}></a>
+                <div class="w-1/6">
+                    <div><a class="text-blue-500" target="_blank" rel="noopener noreferrer" href={`https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=${sraAccession}`}>Trace Link</a></div>
+                </div>
+                <div class="w-1/3">
+                    <div><a class="text-blue-500" href={`https://s3.amazonaws.com/lovelywater/bam/${sraAccession}.bam`} download>Download BAM File</a></div>
+                </div>
             </div>
+            <div class="w-full">Study: {entrezStudyName}</div>
+            <div class="w-1/2">Families:
+                <ul>
+                    {summaryJson.families ?
+                        summaryJson.families.map(family => (
+                            <li key={family.family}>{family.family}</li>
+                        )) : <li>Loading...</li>
+                    }
+                </ul>
             </div>
+            <div class="w-1/2">
+                <img src={heatMap} className="p-6"></img>
+            </div>
+        </div>
+    )
+
+    return (
+        <div class="m-6">
+            {searchBox}
+            {sraAccession ? reportContent : <div></div> }
         </div>
     )
 }
