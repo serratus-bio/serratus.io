3f0637c5c5edd99aad2b3a0f624e77c40dc2bc60
lohrd <57124913+lohrd@users.noreply.github.com>
2020-07-12 15:59:00 -0400

    Data page update
    - still need to clean up

--- src/components/Visual.js
+++ src/components/Visual.js
@@ -1,30 +1,252 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { select } from 'd3';
+import * as d3 from 'd3';
+import DataSdk from '../SDK/DataSdk';
 
 const Visual = (props) => {
     const svgRef = useRef();
     const [data, setData] = useState([100, 25, 35, 45, 85]);
+    const [summaryJson, setSummaryJson] = useState({});
+    const dataSdk = new DataSdk();
+    const canvasRef = useRef();
+    // set the dimensions and margins of the graph
+    // var margin = {top: 30, right: 30, bottom: 30, left: 100},
+    // width = 600 - margin.left - margin.right,
+    // height = 450 - margin.top - margin.bottom;
+
+    // var svg = d3.select(svgRef.current)
+    // .append("svg")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
+    // .append("g")
+    // .attr("transform",
+    //         "translate(" + margin.left + "," + margin.top + ")");
+
+    const getJson = async () => {
+        const summaryJsonFromAPI = await dataSdk.getSraByName("DRR000745");
+        setSummaryJson(summaryJsonFromAPI);
+        console.log(summaryJsonFromAPI)
+    }
+
+    function drawHeatmap(summary, selector) {
+        var cvgCartoonMap = {
+            '_': 0,
+            '.': 0.25,
+            'o': 0.5,
+            'O': 1
+        }
     
+        function getFamilyCoverageData(family) {
+            var familyCoverageData = [];
+            [...family.cvg].forEach(function(bit, i) {
+                familyCoverageData.push({
+                    family: family.family,
+                    bin: i,
+                    cartoonChar: bit
+                })
+            })
+            return familyCoverageData;
+        }
+    
+        function getAllAccessionCoverageData(summary) {
+          var accessionCoverageData = [];
+          summary["accessionSections"].forEach(function(accession, i) {
+              [...accession.cvg].forEach(function(bit, i) {
+                  accessionCoverageData.push({
+                      accession: accession.acc,
+                      family: accession.fam,
+                      bin: i,
+                      cartoonChar: bit
+                  })
+              })
+          });
+          return accessionCoverageData;
+        }
+    
+        // adapted from http://bl.ocks.org/syntagmatic/e8ccca52559796be775553b467593a9f
+        function drawLegend(colorscale) {
+            var legendheight = 200,
+                legendwidth = 80,
+                margin = {top: 10, right: 60, bottom: 10, left: 2};
+    
+            var canvas = legendDiv
+                .style("height", legendheight + "px")
+                .style("width", legendwidth + "px")
+                .style("position", "relative")
+                .append("canvas")
+                .attr("height", legendheight - margin.top - margin.bottom)
+                .attr("width", 1)
+                .style("height", (legendheight - margin.top - margin.bottom) + "px")
+                .style("width", (legendwidth - margin.left - margin.right) + "px")
+                .style("border", "1px solid #000")
+                .style("position", "absolute")
+                .style("top", (margin.top) + "px")
+                .style("left", (margin.left) + "px")
+                .node();
+    
+            var legendscale = d3.scaleLinear()
+                .range([1, legendheight - margin.top - margin.bottom])
+                .domain(colorscale.domain().reverse());
+    
+            var ctx = canvas.getContext("2d");
+            d3.range(legendheight).forEach(function(i) {
+                ctx.fillStyle = colorscale(legendscale.invert(i));
+                ctx.fillRect(0,i,1,1);
+            });
+    
+            var legendaxis = d3.axisRight()
+                .scale(legendscale)
+                .tickSize(6)
+                .ticks(4);
+    
+            var svg = legendDiv
+                .append("svg")
+                .attr("height", (legendheight) + "px")
+                .attr("width", (legendwidth) + "px")
+                .style("position", "absolute")
+                .style("left", "0px")
+                .style("top", "0px")
+    
+            svg
+                .append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + (legendwidth - margin.left - margin.right + 3) + "," + (margin.top) + ")")
+                .call(legendaxis);
+        };
+    
+        function drawAccessionHeatmap(family) {
+    
+        }
+    
+        function drawFamilyHeatmap(family) {
+            var infoSectionVisible = false;
+            function toggleInfoSection() {
+                infoSection.attr("visibility", infoSectionVisible ? "hidden" : "visible");
+                infoSectionVisible = !infoSectionVisible;
+                familySvg.attr("height", infoSectionVisible ? sectionHeight + infoSectionHeight : sectionHeight);
+            }
+    
+            var familySvg = chartG.append("svg")
+                .attr("width", sectionWidth)
+                .attr("height", sectionHeight)
+                .attr("border", barBorder.size)
+                .style("display", "block")
+    
+            var heatBar = familySvg.append("g")
+                .attr("transform",
+                      `translate(${sectionMargin.left}, ${sectionMargin.top})`);
+    
+            var x = d3.scaleBand()
+                .range([ 0, barWidth ])
+                .domain(genomeBins)
+                .padding(0.01);
+    
+            var y = d3.scaleBand()
+                .range([ 0, barHeight ])
+                .domain([family.family])
+                .padding(0.01);
+            var yAxis = heatBar.append("g")
+                .call(d3.axisLeft(y));
+            yAxis.select("path").remove();
+            yAxis.select("line").remove();
+            yAxis.select("text").style('cursor', 'pointer').on("click", function() {
+                toggleInfoSection()
+            });
+    
+            var heatSquares = heatBar.selectAll()
+                .data(getFamilyCoverageData(family))
+                .enter()
+                .append("rect")
+                .attr("x", d => x(d.bin) )
+                .attr("y", d => y(d.family) )
+                .attr("width", x.bandwidth() )
+                .attr("height", y.bandwidth() )
+                .style("fill", d => colorMap(cvgCartoonMap[d.cartoonChar]) )
+    
+            var barBorderPath = heatBar
+                .append("rect")
+                .attr("width", barWidth)
+                .attr("height", barHeight)
+                .style("stroke", barBorder.color)
+                .style("fill", "none")
+                .style("stroke-width", barBorder.size);
+    
+            var infoSection = heatBar.append("g")
+                .attr("visibility", "hidden")
+                .attr("transform",
+                      `translate(0, ${sectionMargin.top + barHeight + 15})`);
+    
+            infoSection.append("text")
+                .text(`TODO: display additional data for ${family.family}.`);
+    
+            // uncomment to pre-load as expanded
+            // toggleInfoSection();
+        }
+    
+        var sectionWidth = 600;
+        var sectionHeight = 20;
+        var infoSectionHeight = 50;
+    
+        var sectionMargin = {top: 2, right: 100, bottom: 2, left: 100};
+        var barWidth = sectionWidth - sectionMargin.left - sectionMargin.right;
+        var barHeight = sectionHeight - sectionMargin.top - sectionMargin.bottom;
+        var barBorder = {size: 1, color: '#999'};
+    
+        var colorMap = d3.scaleSequential(d3.interpolateYlOrRd)
+            .domain([0, 1]);
+    
+        var cvgLength = summary["familySections"][0]["cvg"].length;
+        var genomeBins = [...Array(cvgLength).keys()];
+        var accessionCoverageData = getAllAccessionCoverageData(summary); // n=8725
+    
+        var chartDiv = d3.select(selector)
+            .append("div")
+            .style("display", "inline-block");
+        var chartG = chartDiv.append("g");
+        var legendDiv = d3.select(selector)
+            .append("div")
+            .style("display", "inline-block");
+        drawLegend(colorMap);
+    
+        summary["familySections"].forEach(family => {
+            drawFamilyHeatmap(family);
+        });
+    }
+
+    async function draw() {
+        console.log(summaryJson)
+        drawHeatmap(summaryJson, svgRef.current);
+      }
+      
     useEffect(() => {
-        const svg = select(svgRef.current)
-        svg.selectAll("circle").data(data)
-            .join("circle")
-                .attr("r", value => value)
-                .attr("cx", value => value * 2)
-                .attr("cy", value => value * 2)
-                .attr("stroke", "red")   
-    }, [data]);
+        // const svg = select(svgRef.current)
+        // svg.selectAll("circle").data(data)
+        //     .join("circle")
+        //         .attr("r", value => value)
+        //         .attr("cx", value => value * 2)
+        //         .attr("cy", value => value * 2)
+        //         .attr("stroke", "red")   
+
+        //Read the data
+        // draw();
+      
+    }, []);
 
     return (
-        <div>
-            <svg ref={svgRef}></svg>
+        <div className="flex flex-col justify-center items-center">
+            <svg ref={svgRef}>
+
+                
+            </svg>
+            <canvas ref={canvasRef}></canvas>
             <br/>
-            <button className="p-2" onClick={() => setData(data.map(value => value + 5))}>
-                Enlarge
+            <div className="flex flex-row">
+            <button className="p-2" onClick={() => getJson()}>
+                Get JSON
             </button>
-            <button className="p-2" onClick={() => setData(data.filter(value => value < 45))}>
-                filter
+            <button className="p-2" onClick={() => draw()}>
+                Make Map
             </button>
+            </div>
         </div>
     )
 }
--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DataSdk from '../SDK/DataSdk';
+import Visual from "../components/Visual";
 
 const Data = (props) => {
     var accessionFromParam = new URLSearchParams(props.location.search).get("accession");
@@ -7,7 +8,7 @@ const Data = (props) => {
     const [inputAccession, setInputAccession] = React.useState("");
     const [heatMap, setHeatMap] = React.useState();
     const [summaryJson, setSummaryJson] = React.useState({});
-    const [entrezStudyName, setEntrezStudyName] = React.useState("Loading...");
+    const [entrezStudyName, setEntrezStudyName] = React.useState("");
     const dataSdk = new DataSdk();
 
     async function fetchSerratusApiData(ignore) {
@@ -19,10 +20,12 @@ const Data = (props) => {
           setSummaryJson(summaryJson);
           setHeatMap(sraHeatMap);
           console.log("Done fetching Serratus API data.");
+          console.log(summaryJson);
         }
     }
 
     async function fetchEntrezData(ignore) {
+        setEntrezStudyName("Loading...")
         console.log("Fetching Entrez data...");
         let entrezStudyName = await dataSdk.getEntrezData(sraAccession);
         if (!ignore) {
@@ -48,8 +51,8 @@ const Data = (props) => {
     }
 
     let searchBox = (
-        <div class="flex flex-col items-center">
-            <div class="flex-row">
+        <div class="flex flex-col items-center z-10 mt-2">
+            <div class="flex-row z-10">
                 <input class="rounded border-2 border-gray-300 px-2 m-1" type="text" placeholder="Enter SRA Accession ID" onChange={e => setInputAccession(e.target.value)} />
                 <button onClick={redirect(inputAccession)} class="rounded bg-blue-500 text-white font-bold py-1 px-4" type="submit">Go</button>
             </div>
@@ -89,19 +92,66 @@ const Data = (props) => {
         </div>
     )
 
+    let pageLinks = (
+        <div className="flex flex-col justify-center items-center">
+            <div id="external-links" class="flex flex-row w-1/2 justify-between">
+                <div><a class="text-blue-500" target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/sra/?term=${sraAccession}`}>SRA Link</a></div>
+                <div><a class="text-blue-500" target="_blank" rel="noopener noreferrer" href={`https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=${sraAccession}`}>Trace Link</a></div>
+                <div><a class="text-blue-500" href={`https://s3.amazonaws.com/lovelywater/bam/${sraAccession}.bam`} download>Download BAM File</a></div>
+            </div>
+        </div>
+    )
+
     return (
         // <div class="m-6">
         //     {searchBox}
         //     {sraAccession ? reportContent : <div></div> }
         // </div>
         <div>
-            <div className="h-screen w-screen flex flex-col items-center justify-center">
+            
+            <div className="h-screen w-screen flex flex-col items-center justify-center pt-6 pb-24">
         {/* <Visual></Visual> */}
             <img src="/serratus.jpg" className="invisible sm:visible opacity-75 sm:fixed" style={{objectFit: 'cover', minWidth: '100vh', minHeight: '100vh'}} />
-                <div class="w-2/3 h-full mt-24 mb-24 flex flex-col justify-center items-center z-10 bg-white rounded-lg shadow-2xl">
-                    <div className="w-4/5 bg-gray-500 h-full"></div>
-                    <div className="w-4/5 bg-gray-600 h-full"></div>
-                    <div className="w-4/5 bg-gray-700 h-full"></div>
+            
+            <div class="w-3/4 max-h-full  flex flex-col justify-center items-center z-10 bg-blue-400 border border-gray-600 rounded-lg p-4 shadow-2xl bg-opacity-25">
+                    <div className="w-5/6 bg-gray-400 h-64 border rounded-lg border-gray-600 shadow-xl m-1 z-20">
+                    {searchBox}
+                    {sraAccession ? pageLinks : <div></div> }
+                    <div class="w-full text-center text-xl">{entrezStudyName ? <div className="text-xl italic">Study: {entrezStudyName}</div> : <div></div>}
+                    </div>
+                    </div>
+                    <div className="flex flex-col justify-center items-center w-5/6 bg-gray-400 max-h-full border rounded-lg border-gray-600 shadow-xl m-1">
+
+                    <div class="w-1/2">
+                    
+                        <Visual></Visual>
+                        {/* <img src={heatMap} className="p-6"></img> */}
+                    </div>
+                    </div>
+                    <div className="flex flex-col h-full w-5/6 bg-gray-400 border rounded-lg border-gray-600 shadow-xl m-1 overflow-y-auto">
+                            <div className="text-center text-xl border-gray-800 border-1 p-1 italic">Families:</div>
+                            <div className="overflow-y-auto">
+                            
+                                <div className="h-full p-2">
+                                    {summaryJson.families ?
+                                        summaryJson.families.map(family => (
+                                        <div onClick={() => console.log("hi")} key={family.family} className="flex flex-row justify-between border rounded-md bg-gray-300 border-gray-700 p-2 italic m-1 cursor-pointer">
+                                                <div>{family.family} </div>
+                                                <div> Cvg: {family.cvg} </div>
+                                                <div> Score: {family.score} </div>
+                                                <div> PctId: {family.pctid} </div>
+                                        </div>
+                                        )) : <div>Loading...</div>
+                                    }
+                                </div>
+                        </div>
+                    </div>
+         
+
+
+
+
+
                 {/* <div class="flex flex-col justify-center items-center">
                     {data.sra 
                     ? 
