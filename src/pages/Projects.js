03cca56504138c82ea71f29f3d677a719a711426
lohrd <57124913+lohrd@users.noreply.github.com>
2020-07-12 20:37:19 -0400

    fix padding

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
             <div>
         <div className="flex absolute w-full h-screen justify-center items-center overflow-y-visible sm:overflow-y-hidden overflow-x-hidden">
           <img src="/serratus.jpg" className="invisible sm:visible opacity-75 sm:fixed" style={{objectFit: 'cover', minWidth: '100vh', minHeight: '100vh'}} />
-              <div className="flex flex-col sm:flex-row absolute mt-48 mb-40 pt-56 pb-32 sm:mt-10 p-6 sm:p-0">
+              <div className="flex flex-col sm:flex-row absolute pb-12">
                 <div class="sm:max-w-sm rounded-lg overflow-hidden bg-white shadow-2xl mt-40 sm:m-10 sm:pt-0">
                 <div class="w-full flex flex-col justify-center items-center p-10">
                 <img class="h-24 w-24" src="/db.png" alt="Muzer Logo"/>
