ec3c5c83a309da363ee214c5edc72a65bfcc4f56
Victor Lin <victor@victorl.in>
2020-07-17 18:31:41 -0400

    Improved visual states in report component
    - add message for loading
    - add message for submitting a GH issue if no summary is found

--- src/components/Heatmap.jsx
+++ src/components/Heatmap.jsx
@@ -4,26 +4,57 @@ import * as d3 from 'd3';
 import DataSdk from '../SDK/DataSdk';
 import { drawHeatmap } from '../SDK/D3Heatmap.js';
 
+const dataSdk = new DataSdk();
+
 const Heatmap = (props) => {
     var accession = props.accession;
-    const dataSdk = new DataSdk();
+    const [summaryJson, setSummaryJson] = React.useState(null);
+    const [isLoading, setIsLoading] = React.useState(true);
 
-    async function plot() {
-        let summaryJson = await dataSdk.fetchAccessionJSON(accession);
-        var faux = props.connectFauxDOM('div', 'chart');
-        drawHeatmap(d3, faux, summaryJson);
-    }
-
-    React.useEffect(() => {
+    async function initialRender() {
         if (!accession) {
             return;
         }
-        plot();
+
+        let summaryJson = await dataSdk.fetchAccessionJSON(accession);
+        setSummaryJson(summaryJson);
+        setIsLoading(false);
+
+        if (summaryJson) {
+            var faux = props.connectFauxDOM('div', 'chart');
+            drawHeatmap(d3, faux, summaryJson);
+        }
+    }
+
+    React.useEffect(() => {
+        initialRender();
     }, []);
+
+    let loading = (
+        <div className="text-center">
+            Loading report...
+        </div>
+    )
+
+    let noReport = (
+        <div className="text-center">
+            <span>This accession has not been processed... yet.</span><br />
+            <span>To request this sample be processed, please </span>
+            <a href="https://github.com/ababaian/serratus/issues/new" target="_blank" className="text-blue-600">
+                submit an issue on GitHub
+            </a>
+            <span>.</span>
+        </div>
+    )
 
     return (
         <div>
-            {props.chart}
+            {isLoading ?
+                loading :
+                summaryJson ?
+                    props.chart :
+                    noReport
+            }
         </div>
     )
 }
