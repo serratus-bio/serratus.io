307459d9b30cf09238ca3e1f61182902fe85b5dc
Victor Lin <victor@victorl.in>
2020-07-18 10:22:03 -0400

    Responsive Technology page

--- src/components/TechnologyCard.jsx
+++ src/components/TechnologyCard.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const TechnologyCard = (props) => {
     return (
-        <div className="md:max-w-sm rounded-lg overflow-hidden bg-white shadow-2xl mt-40 md:m-10 md:pt-0">
+        <div className="lg:max-w-sm md:max-w-xl rounded-lg overflow-hidden bg-white md:shadow-2xl mt-40 md:m-10 md:pt-0">
             <img className="h-24 w-24 mt-4 m-auto" src={props.imgTop} alt={props.imgTopAlt} />
             <div className="m-6">
                 <div className="font-bold text-xl mb-2 text-center">{props.title}</div>
                 <p className="text-gray-700 text-base">{props.text}</p>
             </div>
-            <div className="p-4 mx-10 justify-center items-center">
+            <div className="p-4 mx-10">
                 <a target="_blank" rel="noopener noreferrer" href={props.imgBottomLink} >
-                    <img className="m-auto h-20" src={props.imgBottom} alt={props.imgBottomAlt} />
+                    <span className="inline-block align-middle h-full"></span>
+                    <img className="align-middle m-auto h-20" src={props.imgBottom} alt={props.imgBottomAlt} />
                 </a>
             </div>
         </div>
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -4,17 +4,18 @@ import TechnologyCard from '../components/TechnologyCard';
 const Technology = () => {
   return (
     <div>
-      <div className="flex absolute w-full h-screen justify-center items-center overflow-y-visible sm:overflow-y-hidden overflow-x-hidden">
-        <img src="/serratus.jpg" alt="serratus mountain" className="invisible sm:visible opacity-75 sm:fixed" style={{ objectFit: 'cover', minWidth: '100vh', minHeight: '100vh' }} />
-        <div className="flex flex-col md:flex-row absolute pb-12">
+      <div className="flex absolute w-full h-screen justify-center lg:items-center overflow-y-visible overflow-x-hidden">
+        <img src="/serratus.jpg" alt="serratus mountain" className="hidden sm:block opacity-75 sm:fixed" style={{ objectFit: 'cover', minWidth: '100vh', minHeight: '100vh' }} />
+        <div className="flex flex-col lg:flex-row absolute pb-12">
           <TechnologyCard
             title="NCBI SRA Database"
-            text="The NCBI SRA database contains over 1 million RNA-seq libraries amounting to a whopping 5.72 petabytes (yes, that prefix is right, peta-bytes) of data. It is our goal to reanalyze all of these sequences utilizing our ultra-high throughput analysis pipeline hosted on Aamazon Web Service's (AWS) Cloud Computing."
+            text="The NCBI SRA database contains over 1 million RNA-seq libraries amounting to a whopping 5.72 petabytes (yes, that prefix is right, peta-bytes) of data. It is our goal to reanalyze all of these sequences utilizing our ultra-high throughput analysis pipeline hosted on Amazon Web Service's (AWS) Cloud Computing."
             imgTop="/db.png"
             imgTopAlt="database"
             imgBottom="/ncbi.png"
             imgBottomAlt="NCBI logo"
             imgBottomLink="https://www.ncbi.nlm.nih.gov/sra" />
+          <hr className="block md:hidden" />
           <TechnologyCard
             title="Cloud Architecture"
             text="Since there is a lot of data, we need a lot of computers. AWS allows us to spin up hundreds of them at a single time to process data in parallel, utilizing the same bioinformatics pipeline on each of them. Serratus is currently operational, scaling up to 250 nodes with real time tracking and cluster performance analysis, but we are not stopping there..."
@@ -23,6 +24,7 @@ const Technology = () => {
             imgBottom="/aws-logo.png"
             imgBottomAlt="AWS logo"
             imgBottomLink="https://github.com/ababaian/serratus/wiki/Architecture-and-Pipeline" />
+          <hr className="block md:hidden" />
           <TechnologyCard
             title="CoV Sequence Database"
             text="Once serratus is done, all resulting alignment files will be stored in a free, immediate access public database. It is our goal to provide accurate, honest data in order to continue the collaborative effort in fighting COVID-19."
