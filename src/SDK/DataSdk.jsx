1405a8a0d523071b87498c66b19ff0aa932cea0b
Victor Lin <victor@victorl.in>
2020-07-20 20:29:37 -0400

    update DB calls, add entrez query for explore page

--- src/SDK/DataSdk.jsx
+++ src/SDK/DataSdk.jsx
@@ -2,8 +2,13 @@ import axios from 'axios'
 
 export default class DataSdk {
     async getSraByName(sraName) {
-        const response = await axios.get(`https://serratustest-dev-sumdb.us-east-1.elasticbeanstalk.com/api/runs/get-run/${sraName}`);
+        const response = await axios.get(`https://db.serratus.io/api/runs/get-run/${sraName}`);
         return response.data
+    }
+
+    async fetchSraHitsByAccession(genbankAccession) {
+        const response = await axios.get(`https://db.serratus.io/api/runs/get-runs/${genbankAccession}`);
+        return response.data;
     }
 
     async fetchAccessionJSON(accession) {
@@ -54,6 +59,33 @@ export default class DataSdk {
                 .querySelector('Study')
                 .getAttribute('name')
             return entrezStudyName;
+        }
+        catch (err) {
+            return;
+        }
+    }
+
+    async tryGetGenBankTitle(accession) {
+        try {
+            // eSearch
+            let db = "nuccore"
+            let response = await this.getEsearch(db, accession);
+            let parser = new DOMParser();
+            let esearchResults = parser.parseFromString(response, 'text/xml');
+            let entrezId = esearchResults
+                .querySelector("eSearchResult")
+                .querySelector("IdList")
+                .querySelector("Id")
+                .textContent;
+            // eSummary
+            response = await this.getEsummary(db, entrezId);
+            let esummaryResults = parser.parseFromString(response, 'text/xml');
+            let title = esummaryResults
+                .querySelector("eSummaryResult")
+                .querySelector("DocSum")
+                .querySelector("Item[Name=Title]")
+                .textContent;
+            return title;
         }
         catch (err) {
             return;
