4fe2c9101e2a8ec6110672ab7f5943802c834a82
Victor Lin <victor@victorl.in>
2020-07-20 21:01:17 -0400

    use Query page as entry point to data exploration

--- src/components/QueryIntro.jsx
+++ src/components/QueryIntro.jsx
@@ -4,8 +4,9 @@ const QueryInfo = () => {
     return (
         <div className="text-center">
             Explore Serratus SRA run hits for a GenBank accession.<br />
+            Example: <a className="text-blue-600" href="?accession=EU769558.1">EU769558.1</a><br />
             <br />
-            Example: <a className="text-blue-600" href="?accession=EU769558.1">EU769558.1</a>
+            Alternatively, visit the <a className="text-blue-600" href="/report">Report lookup page</a> to access a report for a SRA run.
         </div>
     )
 }
