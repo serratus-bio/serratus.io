95e964186e5e1cdcbc2e58bc833b1e1ba0e7329a
Victor Lin <victor@victorl.in>
2020-07-20 20:50:38 -0400

    add padding to link button

--- src/pages/Query.jsx
+++ src/pages/Query.jsx
@@ -55,7 +55,7 @@ const Query = (props) => {
     let externalLinkIcon = (<svg className="inline fill-current w-4 h-4 ml-1 mb-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,21H5c-1.1,0-2-0.9-2-2V5c0-1.1,0.9-2,2-2h7v2H5v14h14v-7h2v7C21,20.1,20.1,21,19,21z" /><path d="M21 10L19 10 19 5 14 5 14 3 21 3z" /><path d="M6.7 8.5H22.3V10.5H6.7z" transform="rotate(-45.001 14.5 9.5)" /></svg>)
 
     let pageLinks = (
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center my-3">
             <LinkButton
                 link={`https://www.ncbi.nlm.nih.gov/nuccore/${genbankAccession}`}
                 text="GenBank"
