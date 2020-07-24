a5a489c5de7289d6744bad7a48dde8e94a94cf85
Victor Lin <victor@victorl.in>
2020-07-24 18:55:07 -0400

    add top/bottom padding to query result element

--- src/pages/Query.jsx
+++ src/pages/Query.jsx
@@ -120,7 +120,7 @@ const Query = (props) => {
                     }
                 </div>
                 <div className="w-full lg:w-5/6 flex flex-col flex-1 justify-center items-center bg-gray-400 border rounded-lg border-gray-600 shadow-xl m-1 sm:px-12">
-                    <div className="w-full flex flex-col overflow-y-auto" style={{ height: 600 }} id="style-2">
+                    <div className="w-full flex flex-col overflow-y-auto py-6" style={{ height: 600 }} id="style-2">
                         {queryValueStatic ?
                             <QueryResult type={queryTypeStatic} value={queryValueStatic} dataPromise={dataPromise} /> :
                             <QueryIntro />
