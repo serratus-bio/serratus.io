4fb4df54efacf662c1914754606bf57a156046dc
lohrd <57124913+lohrd@users.noreply.github.com>
2020-07-27 13:10:47 -0400

    Update Paginator.jsx

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -3,12 +3,19 @@ import React from "react";
 const Paginator = ( {pageNumber, setPageNumber} ) => {
 
     return (
-    <div>
-        {pageNumber}
+    <div className="flex flex-row justify-center items-center">
+        
         {pageNumber == 1 
-        ? <div><button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button></div> 
-        : <div><button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
-          <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button></div> 
+        ? <div>
+            <button className="invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
+            {pageNumber}
+            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
+          </div> 
+        : <div>
+            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
+            {pageNumber}
+            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
+          </div> 
           }
     </div>
     )
