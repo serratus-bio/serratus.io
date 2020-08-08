ed040fbd97da0bc6e1067e85a35f7c74a214aaea
Victor Lin <victor@victorl.in>
2020-08-08 14:36:30 -0400

    Query: match style of Explore

--- src/pages/Query.jsx
+++ src/pages/Query.jsx
@@ -95,47 +95,46 @@ const Query = (props) => {
         </Helmet>
     )
 
+    var classesBox = " w-full m-auto md:w-3/4 lg:w-1/2 ";
+    var classesBoxBorder = " sm:border sm:rounded sm:border-gray-400 sm:bg-gray-100 ";
+
     return (
-        <div className="flex absolute w-screen h-screen justify-center">
+        <div className="p-4 min-h-screen sm:bg-gray-200">
             {headTags}
-            <img src="/serratus.jpg" alt="serratus mountain" className="hidden sm:block opacity-75 sm:fixed" style={{ objectFit: 'cover', minWidth: '100vh', minHeight: '100vh' }} />
-            <div className="flex flex-col justify-center items-center w-full z-10 rounded-lg p-1
-                sm:shadow-2xl
-                lg:w-3/4 lg:mt-6 lg:bg-blue-400 lg:bg-opacity-25 lg:border lg:border-gray-600">
-                <div className="w-full lg:w-5/6 bg-gray-400 border rounded-lg border-gray-600 sm:shadow-xl p-1 z-20 m-1">
-                    <div className="flex flex-col items-center z-10 mt-2">
-                        <div className="items-center z-10">
-                            <div>
-                                <InputOption className="inline mx-2" value="family" displayText="Family" checked={searchType === "family"} onChange={queryTypeChange} />
-                                <InputOption className="inline mx-2" value="genbank" displayText="GenBank" checked={searchType === "genbank"} onChange={queryTypeChange} />
-                                <InputOption className="inline mx-2" value="run" displayText="SRA Run" checked={searchType === "run"} onChange={queryTypeChange} />
-                            </div>
-                            <input className="rounded border-2 border-gray-300 px-2 m-1 sm:w-64 focus:border-blue-300 focus:outline-none" type="text" placeholder={placeholderText} onKeyUp={searchOnKeyUp} />
-                            <button onClick={() => loadQueryPage(searchValue)} className="rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4" type="submit">Go</button>
+            <div className={"p-4" + classesBox + classesBoxBorder}>
+                <div className="flex flex-col items-center z-10 mt-2">
+                    <div className="items-center z-10">
+                        <div>
+                            <InputOption className="inline mx-2" value="family" displayText="Family" checked={searchType === "family"} onChange={queryTypeChange} />
+                            <InputOption className="inline mx-2" value="genbank" displayText="GenBank" checked={searchType === "genbank"} onChange={queryTypeChange} />
+                            <InputOption className="inline mx-2" value="run" displayText="SRA Run" checked={searchType === "run"} onChange={queryTypeChange} />
                         </div>
+                        <input className="rounded border-2 border-gray-300 px-2 m-1 sm:w-64 focus:border-blue-300 focus:outline-none" type="text" placeholder={placeholderText} onKeyUp={searchOnKeyUp} />
+                        <button onClick={() => loadQueryPage(searchValue)} className="rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4" type="submit">Go</button>
                     </div>
-                    <div className="w-full text-center">
-                        {queryValueStatic ?
-                            <div>
-                                <div className="text-xl font-bold">{queryValueStatic}</div>
-                                {pageTitle ?
-                                    <div className="text-lg italic">{pageTitle}</div> : null}
-                            </div> : null
-                        }
-                    </div>
+                </div>
+                <div className="w-full text-center">
                     {queryValueStatic ?
-                        <div className="flex justify-center items-center my-2">
-                            {getPageLinks(queryTypeStatic, queryValueCorrected)}
+                        <div>
+                            <div className="text-xl font-bold">{queryValueStatic}</div>
+                            {pageTitle ?
+                                <div className="text-lg italic">{pageTitle}</div> : null}
                         </div> : null
                     }
                 </div>
-                <div className="w-full lg:w-5/6 flex flex-col flex-1 justify-center items-center bg-gray-400 border rounded-lg border-gray-600 shadow-xl m-1 sm:px-12">
-                    <div className="w-full flex flex-col overflow-y-auto py-6" style={{ height: 600 }} id="style-2">
-                        {queryValueStatic ?
-                            <QueryResult type={queryTypeStatic} value={queryValueStatic} dataPromise={dataPromise} /> :
-                            <QueryIntro />
-                        }
-                    </div>
+                {queryValueStatic ?
+                    <div className="flex justify-center items-center my-2">
+                        {getPageLinks(queryTypeStatic, queryValueCorrected)}
+                    </div> : null
+                }
+            </div>
+            <div className="sm:h-3"></div>
+            <div className={"p-4" + classesBox + classesBoxBorder}>
+                <div className="w-full flex flex-col p-6">
+                    {queryValueStatic ?
+                        <QueryResult type={queryTypeStatic} value={queryValueStatic} dataPromise={dataPromise} /> :
+                        <QueryIntro />
+                    }
                 </div>
             </div>
         </div>
