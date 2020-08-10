1ff18caeb8a021d2ca3abe33321093541ff2872d
Victor Lin <victor@victorl.in>
2020-08-10 12:17:21 -0400

    add NODE_ENV, google analytics tracking

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 import App from './App';
 
+var history = createBrowserHistory();
+
+if (process.env.NODE_ENV === "production") {
+  ReactGA.initialize('UA-175034803-1');
+  history.listen(location => ReactGA.pageview(location.pathname));
+}
+
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById('root')
