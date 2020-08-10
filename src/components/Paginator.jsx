fc22da1a85dea5751ef9e7218f7b490f963bd779
lohrd <57124913+lohrd@users.noreply.github.com>
2020-08-10 19:25:38 -0400

    Create Paginator
    - component receives pageNumber and setPageNumber hook to iterate through page numbers
    - conditional rendering of buttons
    - needs UI update

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 const Paginator = ( {pageNumber, setPageNumber} ) => {
-
     return (
     <div className="flex flex-row justify-center items-center">
-        
         {pageNumber == 1 
         ? <div>
-            <button className="invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
+            <button className="invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center border-8" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
             {pageNumber}
-            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
+            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center border-8" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
           </div> 
         : <div>
             <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
