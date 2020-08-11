03596b7d6a8e7e3dc07939e389553e0fe4cef17b
lohrd <57124913+lohrd@users.noreply.github.com>
2020-08-10 23:06:04 -0400

    Refactor button styling to variables

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 const Paginator = ( {pageNumber, setPageNumber} ) => {
-    return (
+  const buttonClasses = "bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center";
+  const invisibleButton = "invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center";
+  return (
     <div className="flex flex-row justify-center items-center">
         {pageNumber == 1 
         ? <div>
-            <button className="invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
+            <button className={invisibleButton} onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
             {pageNumber}
-            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
+            <button className={buttonClasses} onClick={() => setPageNumber(pageNumber + 1)}>next</button>
           </div> 
         : <div>
-            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
+            <button className={buttonClasses} onClick={() => setPageNumber(pageNumber - 1)}>prev</button>
             {pageNumber}
-            <button className="bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center" onClick={() => setPageNumber(pageNumber + 1)}>next</button>
+            <button className={buttonClasses} onClick={() => setPageNumber(pageNumber + 1)}>next</button>
           </div> 
           }
     </div>
