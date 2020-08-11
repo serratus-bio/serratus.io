1a8e37d14b70fd53d49e2ab8b00d2ecc949fd7b0
lohrd <57124913+lohrd@users.noreply.github.com>
2020-08-11 01:42:50 -0400

    Accept props, add useEffect hook
    - maintain page number count

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Paginator = ( {pageNumber, setPageNumber} ) => {
+const Paginator = ( {pageNumber, setPageNumber, numberOfPages, getNumberOfPages} ) => {
   const buttonClasses = "bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center";
   const invisibleButton = "invisible bg-gray-300 hover:bg-gray-500 mx-2 py-1 px-4 rounded inline-flex items-center";
+  
+  React.useEffect(() => {
+    getNumberOfPages();
+  }, [pageNumber]);
+
   return (
     <div className="flex flex-row justify-center items-center">
         {pageNumber == 1 
