2694af1a05f65fd7da0114052aa3cc6d81ac3013
Victor Lin <victor@victorl.in>
2020-08-12 08:54:23 -0400

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Family from './pages/Family';
 
 const App = () => {
   React.useEffect(() => {
+    // track initial page load
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
