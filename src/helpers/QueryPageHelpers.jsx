340f2d64ad83389420dbbf5da0e5b0324b40748a
Victor Lin <victor@victorl.in>
2020-08-18 18:52:02 -0400

    throw Error object
    per https://eslint.org/docs/rules/no-throw-literal

--- src/helpers/QueryPageHelpers.jsx
+++ src/helpers/QueryPageHelpers.jsx
@@ -136,7 +136,7 @@ export const parseRange = (rangeStr, bounds) => {
     var [low, high] = rangeStr.split("-").map((s) => {
         var intVal = +s;
         if (isNaN(intVal)) {
-            throw "Invalid query parameter value"
+            throw new Error("Invalid query parameter value");
         }
         return intVal;
     });
