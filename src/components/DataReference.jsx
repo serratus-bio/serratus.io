2d0af77ab4c296361ceb7190f3d0fcfc571a4593
Victor Lin <victor@victorl.in>
2020-08-29 14:22:08 -0400

    Update styling on data reference text

--- src/components/DataReference.jsx
+++ src/components/DataReference.jsx
@@ -3,7 +3,7 @@ import { ExternalLink } from '../helpers/common';
 
 export default () => {
     return (
-        <div>
+        <div className="text-gray-700 text-sm">
             <div className="my-2">All Serratus data can be directly accessed following instructions <ExternalLink href="https://github.com/ababaian/serratus/wiki/Access-Data-Release" className="text-blue-600">here</ExternalLink>.</div>
             <div>If this data assists your research, please consider citing us:</div>
             <span className="text-gray-600">Edgar, R. C. <i>et al</i>. Petabase-scale sequence alignment catalyses viral discovery. <i>bioRxiv</i> 2020.08.07.241729 (2020) </span>
