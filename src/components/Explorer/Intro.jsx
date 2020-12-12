039dda56dc7f9e044e6a0c02af6c124b25991de2
Victor Lin <13424970+victorlin@users.noreply.github.com>
2020-12-12 12:48:43 -0500

    Update prod for v0.8.1 (#94)
    * Add Serratus Explorer wiki button (#91)
    * Add wiki link to navbar (#93)

--- src/components/Explorer/Intro.jsx
+++ src/components/Explorer/Intro.jsx
@@ -12,7 +12,7 @@ export default () => {
             Example 1: Frank the Bat (<a className="text-blue-600" href="?run=ERR2756788">ERR2756788</a>)<br />
             Example 2: Ginger the Cat (<a className="text-blue-600" href="?run=SRR7287110">SRR7287110</a>)
             <img className="m-auto h-64" src="/Frank_Ginger.png" alt="Frank and Ginger, the Serratus mascots"></img>
-            Frank and Ginger, the Serratus mascots.
+            <i>Frank and Ginger, the Serratus mascots.</i>
         </div>
     )
 }
