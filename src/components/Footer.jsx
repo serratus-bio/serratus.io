226f1d4e07bc9590a9e3bc7bef25bc6416480630
Victor Lin <victor@victorl.in>
2020-12-29 21:38:07 -0500

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import { ExternalLink } from '../CommonHelpers';
 
-const Footer = () => {
+export default () => {
     return(
-        <footer>
-            This website is still under <a style={{color:"blue"}} href="https://github.com/serratus-bio/serratus.io">active development</a>.
-            If you have any issues or feedback, please <a style={{color:"blue"}} href="https://github.com/serratus-bio/serratus.io/issues/new">submit an issue on GitHub </a>
-            or reach out to <a style={{color:"blue"}} href="mailto:serratusbio@gmail.com">serratusbio@gmail.com</a>.
+        <footer className="bg-gray-100 px-4 py-2 border-t-2 border-gray-300 md:text-center">
+            This website is still under <ExternalLink href="https://github.com/serratus-bio/serratus.io" className="text-blue-600">active development</ExternalLink>.
+            If you have any issues or feedback, please <ExternalLink href="https://github.com/serratus-bio/serratus.io/issues/new" className="text-blue-600">submit an issue on GitHub</ExternalLink>.
             As an open-source project, serratus.io also welcomes any contributors.
         </footer>
     )
 }
-
-export default Footer;
