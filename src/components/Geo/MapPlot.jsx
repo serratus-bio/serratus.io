40cb4388a2b55fe7dc35e2d87626148452911fd6
Victor Lin <victor@victorl.in>
2021-03-11 11:18:22 -0500

    compute jitter on data load, update pointt text

--- src/components/Geo/MapPlot.jsx
+++ src/components/Geo/MapPlot.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Plot from 'react-plotly.js';
 import Plotly from "plotly.js-basic-dist";
-import dataTsv from './rdrp_pos_jitter.tsv';
+import dataTsv from './rdrp_pos.tsv';
 
 const layout = {
     mapbox: { style: "open-street-map", zoom: 1 },
@@ -17,24 +17,31 @@ const MapPlot = ({ setSelectedPoints }) => {
             function (err, rows) {
                 function unpack(rows, key) {
                     return rows.map(function (row) {
+                        if (key === 'coordinate_x' || key === 'coordinate_y') {
+                            // +(0~111) meters per https://www.usna.edu/Users/oceano/pguth/md_help/html/approx_equivalents.htm
+                            return parseFloat(row[key]) + 0.001 * Math.random();
+                        }
                         return row[key];
                     })
                 };
 
                 function getHoverText(rows) {
                     return rows.map(function (row) {
+                        var text = `${row['run_id']}`
+                        text += `<br>lat, lon = (${row['coordinate_y']}, ${row['coordinate_x']})`;
                         if (row['from_text']) {
-                            return `${row['sra_id']}<br>geocoded text: "${row['from_text']}"`;
+                            text += `<br>Inferred location: "${row['from_text']}"`;
                         }
-                        return `${row['sra_id']}`;
+                        return text;
                     })
                 };
 
                 const newData = [{
-                    lon: unpack(rows, 'coord_jx'),
-                    lat: unpack(rows, 'coord_jy'),
+                    lon: unpack(rows, 'coordinate_x'),
+                    lat: unpack(rows, 'coordinate_y'),
                     customdata: rows,
                     text: getHoverText(rows),
+                    hoverinfo: "text",
                     marker: { color: "Maroon", size: 4 },
                     radius: 3,
                     type: "scattermapbox",
