2992f277aab6fca0d04a8db11db5058b78bbb6b3
ababaian <ababaian@bccrc.ca>
2021-03-13 22:24:10 -0800

    Change to blue buttons for info

--- src/components/Geo/index.jsx
+++ src/components/Geo/index.jsx
@@ -19,9 +19,14 @@ const Geo = () => {
         {headTags}
         <div className="text-center text-xl">The Planetary RNA Virome</div>
 
-        <ExternalLink className='ml-2 mb-1' title="We searched 5.7 million public sequencing libraries for the RNA virus hallmark gene, RNA-dependent RNA Polymerase (RdRP). This map shows the location of BioSamples from which an intact RdRP sequence could be recovered and geographical meta-data was present.">{helpIcon}</ExternalLink>
+       <button className="We searched 5.7 million public sequencing libraries for the RNA virus hallmark gene, RNA-dependent RNA Polymerase (RdRP). This map shows the location of BioSamples from which an intact RdRP sequence could be recovered and geographical meta-data was present. A 100-meter randomization is added to all points to avoid overplotting.">
+            Info
+        </button>
 
-        <button class="addMore" title="Use the <b>Box Select</b> or <b>Lasso Select</b> icons in the top-right to retrieve a detailed sample list below the map.">select data</button>
+        <button className="w-full m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4"
+             title="Use the `Box Select` or `Lasso Select` icons in the top-right to retrieve a detailed sample list below the map.">
+            Selecting Data
+        </button>
 
 
         <div className="my-2">
