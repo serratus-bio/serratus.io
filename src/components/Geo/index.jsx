fedb764022799d866b68874b9c446576ad80bf3c
ababaian <ababaian@bccrc.ca>
2021-03-13 22:54:15 -0800

    Move select text below, add help icon

--- src/components/Geo/index.jsx
+++ src/components/Geo/index.jsx
@@ -19,21 +19,15 @@ const Geo = () => {
         {headTags}
         <div className="text-center text-xl">The Planetary RNA Virome</div>
 
-       <button className="We searched 5.7 million public sequencing libraries for the RNA virus hallmark gene, RNA-dependent RNA Polymerase (RdRP). This map shows the location of BioSamples from which an intact RdRP sequence could be recovered and geographical meta-data was present. A 100-meter randomization is added to all points to avoid overplotting.">
-            Info
-        </button>
-
-        <button className="w-full m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4"
-             title="Use the `Box Select` or `Lasso Select` icons in the top-right to retrieve a detailed sample list below the map.">
-            Selecting Data
-        </button>
-
+        <ExternalLink className='ml-2 mb-1' title="We searched 5.7 million public sequencing libraries for the RNA virus hallmark gene, RNA-dependent RNA Polymerase (RdRP). This map shows the location of BioSamples from which an intact RdRP sequence could be recovered and geographical meta-data was present. A 100-meter randomization is applied to all points to prevent overplotting.">{helpIcon}</ExternalLink>
 
         <div className="my-2">
             <MapPlot setSelectedPoints={setSelectedPoints} />
         </div>
 
         <div className="text-center">Beta Version. Feedback is welcome.</div>
+
+        <div class="text-left text-gray-500">Use the `Box Select` or `Lasso Select` icons in the top-right to retrieve a list of sample details below the map.</p>
 
         <SelectionInfo selectedPoints={selectedPoints} />
     </div>
