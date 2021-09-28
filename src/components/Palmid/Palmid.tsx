e9f4d2aef67e0804c7287461b7e502fc747b1b04
ababaian <ababaian@bccrc.ca>
2021-09-28 23:30:25 +0100

    display outputs to understand JS

--- src/components/Palmid/Palmid.tsx
+++ src/components/Palmid/Palmid.tsx
@@ -13,9 +13,10 @@ export const Palmid = () => {
         const fastaInput = document.getElementById('fastaInput') as HTMLInputElement
         let fastaText = fastaInput.value
 
-        var header_text
+        var header_text = '>Serratus_palmid'
         var seq_text
-        var parsed_seq_text 
+        var parsed_seq_text
+        var parsedFasta = '---'
 
         if ( fastaText.search(">") >= 0) {
             // Parse with header
@@ -32,29 +33,50 @@ export const Palmid = () => {
 
         } else {
             // Parse without header
-            header_text = '>Serratus_palmid'
+            //header_text = '>Serratus_palmid'
 
             // All is protein sequence
             parsed_seq_text = fastaText
 
         }
 
-        // Parse Fasta Sequence
+        // Parse Fasta Sequenceg
         // remove all non alpha-characters
         parsed_seq_text = parsed_seq_text.replace(/[^A-Za-z]/g, '').toUpperCase()
 
         // Parsed Fasta Testing (Visual)
-        var parsedFasta = header_text.concat('\n', parsed_seq_text)
+        parsedFasta = header_text.concat('\n', parsed_seq_text)
 
         document.getElementById('faHeader')!.innerHTML = header_text
         document.getElementById('faSeq')!.innerHTML = parsed_seq_text
         document.getElementById('parsedFa')!.innerHTML = parsedFasta
+    }
 
+/*    const postFasta = () => {
+        var newSrc="placeholder_for_response"
+
+        var data_call = { ["sequence"]: sequence_text_right };
+        postData('https://3niuza5za3.execute-api.us-east-1.amazonaws.com/default/api-lambda', data_call)
+        .then(data => {
+        newSrc = "https://s3.amazonaws.com/openvirome.com/" + SHA1(sequence_text_right) + ".html";
+        document.getElementById("iframe").src=newSrc;
+        document.getElementById("download_button").href=newSrc;
+        document.getElementById('iframe').contentWindow.location.reload()}
+      );   
+
+    }
+*/
+
+    const postFasta = () => {
+        var hashResponse="placeholder_for_response"
+        //var fastaData = { ["sequence"]: parsedFasta };
 
     }
 
-    const postFasta = () => {
-
+    const getHash = () => {
+        var hash = document.getElementById('parsedFa')!.innerHTML
+        var hashUrl = '?hash='
+        hashUrl = hashUrl.concat(hash)
     }
 
     return (
@@ -89,9 +111,38 @@ export const Palmid = () => {
                 <p> ============ </p>
                 <p> API Submission Fasta: </p>
                 <p id='parsedFa' ></p>
+                <p> ============ </p>
 
+                <button
+                className='w-300 m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4'
+                onClick={postFasta}>
+                Post Fasta
+                </button>
+                <p>Post API:</p>
+                <p id='postApi'> </p>
 
             </div>
+
+            <div className={`py-4 px-6 mx-4 ${classesBoxBorder}`}>
+            <a className='text-blue-600' href='?hash=ERR2756788'>
+            Frank the Bat (ERR2756788)</a>
+            <p> ---------------------- </p>
+            <button
+                className='w-300 m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4'
+                onClick={postFasta}>
+                Retrieve Report
+            </button>
+            <iframe id="palmReport"
+                title="Inline Frame Example"
+                width="100%"
+                height="400"
+                //src="https://s3.amazonaws.com/openvirome.com/3xample.html"
+                src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+            </iframe>
+            </div>
+
+
+
         </div>
     )
 }
