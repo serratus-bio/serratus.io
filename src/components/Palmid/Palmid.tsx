c982b893ea67757b30541918b41ffde6cbe75563
ababaian <ababaian@bccrc.ca>
2021-10-01 17:09:37 +0100

    Delinting code

--- src/components/Palmid/Palmid.tsx
+++ src/components/Palmid/Palmid.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { classesBoxBorder, ExternalLink } from 'common'
+import { classesBoxBorder } from 'common'
 import axios from 'axios'
 
 // Global variables for webpage
-let PARSEDFASTA ='no_fasta_input'
+let PARSEDFASTA = 'no_fasta_input'
 let FAHASH = 'no_api_response'
 let REPORTURL = 'NA'
 const OV = 'https://s3.amazonaws.com/openvirome.com/'
@@ -12,26 +12,24 @@ const OV = 'https://s3.amazonaws.com/openvirome.com/'
 // API
 const apiUrl = 'https://3niuza5za3.execute-api.us-east-1.amazonaws.com/default/api-lambda'
 
-
 export async function submitFasta(faSubmit: string) {
     const response = await axios.post(`${apiUrl}`, {
-        sequence: faSubmit
+        sequence: faSubmit,
     })
     return response.data
 }
 
 // Fake API call because I'm a newb
 export function fakeSubmit(faSubmit: string) {
-
     // An API call which returns the hash of submit sequence
     // goes here
+    faSubmit = '3xample'
 
-    return '3xample'
+    return faSubmit
 }
 
 export function wrap(s: string) {
-    return s.replace(
-    /(.{50})/g, '$1\n')
+    return s.replace(/(.{50})/g, '$1\n')
 }
 
 // Webpage
@@ -48,11 +46,11 @@ export const Palmid = () => {
         const fastaInput = document.getElementById('fastaInput') as HTMLInputElement
         let fastaText = fastaInput.value
 
-        var header_text = '>Serratus_palmid'
-        var seq_text
-        var parsed_seq_text
+        let header_text = '>Serratus_palmid'
+        let seq_text
+        let parsed_seq_text
 
-        if ( fastaText.search(">") >= 0) {
+        if (fastaText.search('>') >= 0) {
             // Parse with header
             // Parse first sequence in case of multiple fasta
             fastaText = fastaText.split('>')[1]
@@ -64,14 +62,12 @@ export const Palmid = () => {
             seq_text = fastaText.split('\n')
             seq_text.shift()
             parsed_seq_text = seq_text.join()
-
         } else {
             // Parse without header
             //header_text = '>Serratus_palmid'
 
             // All is protein sequence
             parsed_seq_text = fastaText
-
         }
 
         // Parse Fasta Sequenceg
@@ -86,88 +82,74 @@ export const Palmid = () => {
         document.getElementById('faHeader')!.innerHTML = header_text
         document.getElementById('faSeq')!.innerHTML = wrap(parsed_seq_text)
         //document.getElementById('faSubmit')!.innerHTML = PARSEDFASTA
-
     }
 
     const postFasta = () => {
         //PARSEDFASTA = parseFasta()
         FAHASH = fakeSubmit(PARSEDFASTA)
-        document.getElementById('postApi')!.innerHTML =  FAHASH
+        document.getElementById('postApi')!.innerHTML = FAHASH
 
         //Redirect to hash search page
         window.location.search = '?hash='
     }
 
     // Parse URL-Search & Display Report
-
-    var urlParams = new URLSearchParams(document.location.search.substring(1))
-    var pageHash = urlParams.get("hash")
+    let urlParams = new URLSearchParams(document.location.search.substring(1))
+    let pageHash = urlParams.get('hash')
 
     const showReport = () => {
-        if (pageHash == null){
+        if (pageHash == null) {
             // Show Fasta Submission Form
             // Default
 
             // Hide PalmID Report window
-            document.getElementById('palmReport')!.style.height = "0"
-            document.getElementById('palmReport')!.style.width = "0%"
+            document.getElementById('palmReport')!.style.height = '0'
+            document.getElementById('palmReport')!.style.width = '0%'
 
             REPORTURL = '/Frank_Ginger.png'
             document.getElementById('reportUrl')!.innerHTML = REPORTURL
             document.getElementById('palmReport')!.setAttribute('src', REPORTURL)
-
-
-
         } else {
             // Hide Fasta Submission Form
             setIsCollapsed(!isCollapsed)
 
             // Show PalmID Report window
-            document.getElementById('palmReport')!.style.height = "400"
-            document.getElementById('palmReport')!.style.width = "100%"
+            document.getElementById('palmReport')!.style.height = '400'
+            document.getElementById('palmReport')!.style.width = '100%'
 
-
-            REPORTURL = OV.concat(pageHash, ".html")
+            REPORTURL = OV.concat(pageHash, '.html')
             // FOR DEV USE STATIC IMAGE
             REPORTURL = '/Frank_Ginger.png'
-
             document.getElementById('reportUrl')!.innerHTML = REPORTURL
             document.getElementById('palmReport')!.setAttribute('src', REPORTURL)
-
-
         }
-
     }
 
-    window.onload = function() {
-       showReport()
+    window.onload = function () {
+        showReport()
     }
 
     return (
-
         <div className='min-h-screen w-full sm:bg-gray-100 py-4'>
             {headTags}
             {/*HEADER*/}
-            <h1 className='text-3xl font-bold text-center'>
-                palmID: Viral-RdRP Analysis
-            </h1>
+            <h1 className='text-3xl font-bold text-center'>palmID: Viral-RdRP Analysis</h1>
 
             <p> [DEV] URL hash : {pageHash} </p>
 
             {/*FASTA Submission Box*/}
-
             <div id='faSubmissionBox' className={`py-4 px-6 mx-4 ${classesBoxBorder}`}>
-
                 <button
                     className='text-left collapse-button'
                     onClick={() => setIsCollapsed(!isCollapsed)}>
                     [Sequence Submission]
                 </button>
 
-                <div id = 'faSubmissionForm'
+                <div
+                    id='faSubmissionForm'
                     className={`collapse-content ${!isCollapsed ? 'collapsed' : 'expanded'}`}
-                    aria-expanded = {isCollapsed} >
-                    {/*Fasta parsing and submission form*/}               
+                    aria-expanded={isCollapsed}>
+                    {/*Fasta parsing and submission form*/}
                     <p className='my-3'>Sequence, in FASTA format</p>
 
                     <form id='submissionForm'>
@@ -194,27 +176,21 @@ export const Palmid = () => {
                             Analyze Sequence
                         </button>
                     </form>
-
                 </div>
-
                 <p>[DEV] Post API:</p>
                 <p id='postApi'> </p>
-
             </div>
 
             <div className={`py-4 px-6 mx-4 ${classesBoxBorder}`}>
-            <p>[DEV] Report URL: </p>
-            <p id = 'reportUrl'></p>
-
-            <iframe id="palmReport"
-                title="Inline Frame Example"
-                width="25%"
-                height="400"
-                src="/load_palmid.gif">
-            </iframe>
-
+                <p>[DEV] Report URL: </p>
+                <p id='reportUrl'></p>
+                <iframe
+                    id='palmReport'
+                    title='Inline Frame Example'
+                    width='25%'
+                    height='400'
+                    src='/load_palmid.gif'></iframe>
             </div>
-
         </div>
     )
 }
