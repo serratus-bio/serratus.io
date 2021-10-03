452ba1063cc071c43f467e2bc7ca46329460cedd
Binoy Subhakumar <bsubhaku@redhat.com>
2021-10-03 12:52:01 -0500

    improved timer and added clear button

--- src/components/Palmid/Palmid_advanced.tsx
+++ src/components/Palmid/Palmid_advanced.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Iframe from 'react-iframe'
 import { SpinningCircles } from 'react-loading-icons'
 import { useFasta } from './hooks/useFasta'
 
 export const Palmid_advanced = () => {
-    const { fastaHash, isPostFastaLoading, isPostFastaError, getReport, postFasta } = useFasta()
+    const {
+        fastaHash,
+        isPostFastaLoading,
+        isReportReady,
+        checkReport,
+        postFasta,
+        clear,
+    } = useFasta()
     const [fastaInput, setFastaInput] = useState<string>('')
+    const [showIframe, setShowIframe] = useState<boolean>(false)
+    const HALF_MINUTE_MS = 10000
 
-    console.log('fastaHash', fastaHash)
+    useEffect(() => {
+        let interval: any
+        if (fastaHash) {
+            if (!isReportReady) {
+                // first call
+                checkReport()
+
+                //later calls
+                interval = setInterval(() => {
+                    checkReport()
+                }, HALF_MINUTE_MS)
+                return () => clearInterval(interval)
+            } else {
+                // stop the api call to check for report
+                clearInterval(interval)
+            }
+        }
+    }, [fastaHash, isReportReady])
+
     return (
         <>
             <div className='m-4 p-4'>
@@ -27,35 +54,46 @@ export const Palmid_advanced = () => {
                 <button
                     className='w-300 m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4'
                     onClick={async () => {
+                        setShowIframe(true)
                         await postFasta(fastaInput)
                     }}>
                     Analyze Sequence
                 </button>
+                <button
+                    className='ml-4 w-300 m-auto rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4'
+                    onClick={async () => {
+                        setShowIframe(false)
+                        setFastaInput('')
+                        clear()
+                    }}>
+                    Clear
+                </button>
             </div>
-
-            <div className='min-h-screen m-8 p-4 border-2 rounded flex justify-center items-center'>
-                {fastaHash && !isPostFastaLoading ? (
-                    <Iframe
-                        url={`https://s3.amazonaws.com/openvirome.com/${fastaHash}.html`}
-                        width='100%'
-                        id='myId'
-                        className='min-h-screen'
-                        position='relative'
-                    />
-                ) : (
-                    <div className=''>
-                        <SpinningCircles
-                            fill='#0EA5FD'
-                            fillOpacity={1}
-                            width={200}
-                            speed={1}
-                            stroke='#0EA5FD'
-                            strokeOpacity={1}
-                            strokeWidth={2}
+            {showIframe && (
+                <div className='min-h-screen m-8 p-4 border-2 rounded flex justify-center items-center'>
+                    {isReportReady ? (
+                        <Iframe
+                            url={`https://s3.amazonaws.com/openvirome.com/${fastaHash}.html`}
+                            width='100%'
+                            id='myId'
+                            className='min-h-screen'
+                            position='relative'
                         />
-                    </div>
-                )}
-            </div>
+                    ) : (
+                        <div className=''>
+                            <SpinningCircles
+                                fill='#0EA5FD'
+                                fillOpacity={1}
+                                width={200}
+                                speed={1}
+                                stroke='#0EA5FD'
+                                strokeOpacity={1}
+                                strokeWidth={2}
+                            />
+                        </div>
+                    )}
+                </div>
+            )}
         </>
     )
 }
--- src/components/Palmid/hooks/useFasta.tsx
+++ src/components/Palmid/hooks/useFasta.tsx
@@ -3,22 +3,33 @@ interface FastaHook {
     fastaHash: string
     isPostFastaLoading: boolean
     isPostFastaError: boolean
-    getReport: (fastafastaHash: string) => void
+    isReportReady: boolean
+    checkReport: () => void
     postFasta: (rnaSequence: string) => void
+    clear: () => void
 }
 export function useFasta(): FastaHook {
-    const x =
-        '>SRR9968562_waxsystermes_virus_microassembly\nPIWDRVLEPLMRASPGIGRYMLTDVSPVGLLRVFKEKVDTTPHMPPEGMEDFKKASKEVE\nKTLPTTLRELSWDEVKEMIRNDAAVGDPRWKTALEAKESEEFWREVQAEDLNHRNGVCLR\nGVFHTMAKREKKEKNKWGQKTSRMIAYYDLIERACEMRTLGALNADHWAGEENTPEGVSG\nIPQHLYGEKALNRLKMNRMTGETTEGQVFQGDIAGWDTRVSEYELQNEQRICEERAESED\nHRRKIRTIYECYRSPIIRVQDADGNLMWLHGRGQRMSGTIVTYAMNTITNAIIQQAVSKD\nLGNTYGRENRLISGDDCLVLYDTQHPEETLVAAFAKYGKVLKFEPGEPTWSKNIENTWFC\nSHTYSRVKVGNDIRIMLDRSEIEILGKARIVLGGYKTGEVEQAMAKGYANYLLLTFPQRR\nNVRLAANMVRAIVPRGLLPMGRAKDPWWREQPWMSTNNMIQAFNQIWEGWPPISSMKDIK\nYVGRAREQMLDST'
     const [isPostFastaLoading, setIsPostFastaLoading] = useState<boolean>(false)
     const [isPostFastaError, setIsPostFastaError] = useState<boolean>(false)
+    const [isReportReady, setIsReportReady] = useState<boolean>(false)
     const [fastaHash, setFastaHash] = useState<string>('')
 
+    function clear() {
+        setFastaHash('')
+        setIsReportReady(false)
+        setIsPostFastaError(false)
+        setIsPostFastaLoading(false)
+    }
     async function postFasta(rnaSequence: string) {
         try {
+            setIsReportReady(false)
             const response = await fetch(
                 'https://3niuza5za3.execute-api.us-east-1.amazonaws.com/default/api-lambda',
                 {
                     method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
                     body: JSON.stringify({
                         sequence: rnaSequence,
                     }),
@@ -33,12 +44,30 @@ export function useFasta(): FastaHook {
         }
     }
 
-    async function getReport(fastafastaHash: string) {
-        const response = await fetch(
-            `https://s3.amazonaws.com/openvirome.com/${fastafastaHash}.html`
-        )
-        console.log(response)
+    async function checkReport() {
+        try {
+            if (!isReportReady) {
+                const response = await fetch(
+                    `https://s3.amazonaws.com/openvirome.com/${fastaHash}.html`
+                )
+                if (response.status === 200) {
+                    setIsReportReady(true)
+                } else {
+                    setIsReportReady(false)
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    return { fastaHash, isPostFastaLoading, isPostFastaError, getReport, postFasta }
+    return {
+        fastaHash,
+        isPostFastaLoading,
+        isPostFastaError,
+        isReportReady,
+        checkReport,
+        postFasta,
+        clear,
+    }
 }
