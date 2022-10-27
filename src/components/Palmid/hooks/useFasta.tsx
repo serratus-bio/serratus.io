e319208cd56dc692530c72f5e9f85bfe55b79a7f
Hannah Le <hannah.lgbhan@gmail.com>
2022-10-26 22:24:18 -0700

    bump to number of palmid requests

--- src/components/Palmid/hooks/useFasta.tsx
+++ src/components/Palmid/hooks/useFasta.tsx
@@ -33,7 +33,7 @@ export function useFasta(): FastaHook {
     }
 
     useEffect(() => {
-        if (checkReportRequestCount >= 60) {
+        if (checkReportRequestCount >= 120) {
             setIsCheckReportTimedOut(true)
         }
     }, [checkReportRequestCount])
