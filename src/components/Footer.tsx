97685814b40a1781c4de4898cb63d53663b825d2
nahid18 <nahidpatwary50@gmail.com>
2023-08-27 23:32:14 +0600

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import { ExternalLink } from 'common'
 
 export const Footer = () => {
     return (
-        <footer className='bg-gray-50 px-10 py-2 border-t-2 border-gray-200'>
-            <div className='flex gap-1 items-center justify-center'>
+        <footer className='bg-gray-50 px-10 py-2 border-t-2 border-gray-200 md:text-center'>
+            <div>
                 Want to revolutionize virus discovery? Serratus is hiring{' '}
                 <ExternalLink href='https://rrna.ca/id0001' className='text-blue-600'>
                     computational virologists
